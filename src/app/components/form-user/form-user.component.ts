77dc3731c8095a6fcbf13ff1fbeaaf89170e01f0
JulioHerreraSe <julio.herreraseara@gmail.com>
2025-01-27 13:51:53 +0100

    Form implemment

---
+++ src/app/components/form-user/form-user.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'form-user',
+  imports: [FormsModule],
+  templateUrl: './form-user.component.html'
+})
+export class FormUserComponent {
+
+  user: User;
+
+  @Output() newUserEventEmitter: EventEmitter<User> = new EventEmitter();
+
+  constructor() {
+    this.user = new User();
+  }
+
+  onSubmit(userForm: NgForm): void {
+    if(userForm.valid) {
+      this.newUserEventEmitter.emit(this.user);
+      console.log(this.user);
+    }
+    userForm.reset();
+    userForm.resetForm();
+    
+  }
+}
--- src/app/components/user-app.component.ts
+++ src/app/components/user-app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
 import { UserComponent } from './user/user.component';
+import { FormUserComponent } from './form-user/form-user.component';
 
 @Component({
   selector: 'user-app',
-  imports: [UserComponent],
+  imports: [UserComponent, FormUserComponent],
   templateUrl: './user-app.component.html'
 })
 export class UserAppComponent {
@@ -16,6 +17,10 @@ export class UserAppComponent {
 
   constructor(private service: UserService) {
     this.service.findAll().subscribe( users => this.users = users);
+  }
+
+  addUser(user: User) {
+    this.users = [... this.users, {... user, id: new Date().getTime() % 1000 }];
   }
 
 }
